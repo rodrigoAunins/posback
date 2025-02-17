bd3930c725e290b061afe76384ca0905349392f3
rodrigoAunins <rodrigoaunins@outlook.com.ar>
2025-02-17 10:44:29 -0300

    se agrega configuraciones db5

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 import * as bodyParser from 'body-parser';
-import * as nodeCrypto from 'crypto';
-
+// import * as nodeCrypto from 'crypto'; // si no lo usas, no hace falta
 
 async function bootstrap() {
+  // Si usaras randomUUID, harías:
+  // if (!globalThis.crypto) {
+  //   globalThis.crypto = nodeCrypto;
+  // }
+
   const app = await NestFactory.create(AppModule, { cors: true });
-  const configService = app.get(ConfigService);
+
+  // Ya no pedimos el ConfigService
+  // const configService = app.get(ConfigService); // ← quítalo
+
+  // Puedes usar el process.env.PORT o un fallback
   const port = process.env.PORT || 3000;
 
-    // 2) Aumentar límite de JSON y URL-encoded
-    app.use(bodyParser.json({ limit: '50mb' }));
-    app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+  // Aumentar límite de payload
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
   await app.listen(port, () => {
-    console.log(`Nest backend is running on http://localhost:${port}`);
+    console.log(`Nest backend is running on port ${port}`);
   });
 }
-
 
 bootstrap();
